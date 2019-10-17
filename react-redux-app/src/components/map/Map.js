50111414fd50d61e3a8c6175fcb6c96d0c98975e
Awais Kamran <>
2019-10-17 12:53:53 +0500

    react-redux-app: point animation on mapbox updated

--- react-redux-app/src/components/map/Map.js
+++ react-redux-app/src/components/map/Map.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 import MapGL, { Source, Layer } from 'react-map-gl';
+import * as turf from '@turf/turf'
 
 const pointLayer = {
     type: 'circle',
@@ -10,20 +11,29 @@ const pointLayer = {
     }
 };
 
-function pointOnCircle({ center, angle, radius }) {
-    return {
-        type: 'Point',
-        coordinates: [center[0] + Math.cos(angle) * radius, center[1] + Math.sin(angle) * radius]
-    };
+const routeLayer = {
+    id: 'route',
+    source: 'route',
+    type: 'line',
+    paint: {
+        'line-width': 1,
+        'line-color': '#007cbf'
+    }
 }
 
+var factor = 0.0005;
+var originLongitude = -122.4376;
+var originLatitude = 37.7577;
+var counter = 0;
+var steps = 300;
 export default class Map extends Component {
     state = {
         pointData: null,
+        routeData: null,
         viewport: {
-            latitude: 37.7577,
-            longitude: -122.4376,
-            zoom: 17
+            latitude: originLatitude,
+            longitude: originLongitude,
+            zoom: 16
         }
     };
     animation = null;
@@ -40,30 +50,101 @@ export default class Map extends Component {
     _onViewportChange = viewport => this.setState({ viewport });
 
     _animatePoint = () => {
-        this.setState({
-            pointData: pointOnCircle({ center: [-122.4376, 37.7577], angle: Date.now() / 1000, radius: 0.001 })
-        });
+        if (this.state.routeData) {
+            if(counter < steps){
+                this.setState({
+                    pointData: {
+                        type: 'Point',
+                        coordinates: this.state.routeData.features[0].geometry.coordinates[counter++]
+                    }
+                });
+            }
+        } else{
+            this.setState({
+                pointData: {
+                    type: 'Point',
+                    coordinates: [originLongitude, originLatitude]
+                }
+            });
+        }
         this.animation = window.requestAnimationFrame(this._animatePoint);
     };
 
+    _updatePoint = () => {
+        counter = 0;
+        let fromLatitude = originLatitude;
+        originLatitude += factor;
+        let toLatitude = originLatitude + factor;
+        this._getDistance(originLongitude, fromLatitude, toLatitude);
+    }
+
+    _getDistance = (originLongitude, fromLatitude, toLatitude) => {
+        let from = turf.point([originLongitude, fromLatitude]);
+        let to = turf.point([originLongitude, toLatitude]);
+        let options = { units: 'kilometers' };
+        let distance = turf.distance(from, to, options);
+
+        let routeData = {
+            type: 'FeatureCollection',
+            features: [{
+                type: 'Feature',
+                geometry: {
+                    type: 'LineString',
+                    coordinates: [
+                        [originLongitude, fromLatitude],
+                        [originLongitude, toLatitude]
+                    ]
+                }
+            }]
+        }
+        this.setState({ routeData }, function () {
+            this._getArcByDistance(distance);
+        });
+
+        console.log(`The distance from to and from: ${distance} km`);
+    }
+
+    _getArcByDistance = (distance) => {
+        let arc = [];
+        for (let i = 0; i < distance; i += distance / steps) {
+            let segment = turf.along(this.state.routeData.features[0], i, { units: 'kilometers' });
+            arc.push(segment.geometry.coordinates);
+        }
+        console.log(`The arc values are: ${arc}`);
+        let routeData = this.state.routeData;
+        routeData.features[0].geometry.coordinates = arc;
+        this.setState({ routeData }, function () {
+            console.log("Arc updated on the map!");
+        });
+    }
 
     render() {
-        const { viewport, pointData } = this.state;
+        const { viewport, pointData, routeData } = this.state;
 
         return (
-            <MapGL
-                {...viewport}
-                width="1200px"
-                height="750px"
-                mapboxApiAccessToken={this.TOKEN}
-                onViewportChange={this._onViewportChange}
-            >
-                {pointData && (
-                    <Source type="geojson" data={pointData}>
-                        <Layer {...pointLayer} />
-                    </Source>
-                )}
-            </MapGL>
+            <div>
+                <MapGL
+                    {...viewport}
+                    width="1200px"
+                    height="750px"
+                    mapboxApiAccessToken={this.TOKEN}
+                    onViewportChange={this._onViewportChange}
+                >
+                    {pointData && (
+                        <Source type="geojson" data={pointData}>
+                            <Layer {...pointLayer} />
+                        </Source>
+                    )}
+
+                    {routeData && (
+                        <Source type="geojson" data={routeData}>
+                            <Layer {...routeLayer} />
+                        </Source>
+                    )}
+                </MapGL>
+                <button onClick={this._updatePoint}>Click</button>
+            </div>
+
         );
     }
 }
